b2b5a65fef9110f4680d71b440bbf542e4ede0e6
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-06-02 14:05:25 -0400

    Add stub projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>So hey this is where some projects gonna go</h1>
+    </main>
+  );
+}
