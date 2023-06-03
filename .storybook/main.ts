c03d419432254d065460261acdc01494880d6074
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-06-03 12:45:21 -0400

    Add and setup Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+import { resolve } from 'path';
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  async webpackFinal(config) {
+    config.resolve.alias['@'] = resolve(__dirname, '..');
+
+    return config;
+  },
+};
+export default config;
