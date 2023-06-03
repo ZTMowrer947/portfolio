b8c2a79a61294cda415bc220b7b3cdbb45547f91
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-06-03 11:10:14 -0400

    Add randomly generate project card
    Style project cards

--- app/projects/card.tsx
+++ app/projects/card.tsx
@@ -10,23 +10,40 @@ export interface ProjectCardProps {
 export default function ProjectCard({ project }: ProjectCardProps) {
   const firstImage = project.images[0];
 
+  const hasSourceLink = !!project.sourceLink;
+  const hasLiveLink = !!project.liveLink;
+
+  let btnClasses = 'text-center my-2 text-gray-400 hover:text-white ';
+
+  if (hasLiveLink && hasSourceLink) btnClasses += 'col-span-2 border-x';
+  else if (hasLiveLink || hasSourceLink) btnClasses += 'col-span-3 border-x';
+  else btnClasses += 'col-span-4 col-start-2';
+
   return (
-    <article>
-      <h1>{project.title}</h1>
-      <div>
-        <ProjectImg image={firstImage} />
-        <Link href={`/projects/${project.id}`}>Details</Link>
+    <article className="m-2 border-2 rounded flex flex-col align-center">
+      <ProjectImg image={firstImage} />
+      <h1 className="text-center text-lg font-bold">{project.title}</h1>
+      <div className="grid grid-cols-6 border-t">
+        <Link href={`/projects/${project.id}`} className={btnClasses}>
+          Details
+        </Link>
         {project.sourceLink && (
           <a
             href={project.sourceLink}
             target="_blank"
             rel="noreferrer noopener"
+            className={btnClasses}
           >
             Source Code
           </a>
         )}
         {project.liveLink && (
-          <a href={project.liveLink} target="_blank" rel="noreferrer noopener">
+          <a
+            href={project.liveLink}
+            target="_blank"
+            rel="noreferrer noopener"
+            className={btnClasses}
+          >
             Live Demo
           </a>
         )}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,54 @@
+import Chance from 'chance';
+
+import { Project, ProjectImage } from '@/app/projects/type';
+import ProjectCard from '@/app/projects/card';
+
+function generateProjectImage(chance: Chance.Chance): ProjectImage {
+  return {
+    id: chance.guid(),
+    altText: chance.sentence(),
+    smallImg: {
+      src: 'https://placehold.it/640x360',
+      width: 640,
+    },
+    medImg: {
+      src: 'https://placehold.it/800x450',
+      width: 800,
+    },
+    largeImg: {
+      src: 'https://placehold.it/1024x576',
+      width: 1024,
+    },
+    xlargeImg: {
+      src: 'https://placehold.it/1280x720',
+      width: 1280,
+    },
+  };
+}
+
+function generateProject(chance: Chance.Chance): Project {
+  return {
+    id: chance.guid(),
+    title: chance.string({ length: 10, alpha: true }),
+    description: chance.paragraph(),
+    liveLink: chance.bool() ? chance.url({ protocol: 'https' }) : undefined,
+    sourceLink: chance.bool() ? chance.url({ protocol: 'https' }) : undefined,
+    tags: chance.unique(chance.string, 5, { alpha: true, length: 5 }),
+    images: chance.n(generateProjectImage, 3, chance),
+  };
+}
+
 export default function Projects() {
+  const chance = new Chance();
+  const projects = chance.n(generateProject, 10, chance);
+
+  const cards = projects.map((project) => (
+    <ProjectCard project={project} key={project.id} />
+  ));
+
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <h1>So hey this is where some projects gonna go</h1>
+    <main className="p-12 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
+      {cards}
     </main>
   );
 }
