d85ca2b3b87c0624aaebb4f2a9d9c55a82694c67
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-07-11 09:32:58 -0400

    Add hover styling for about page external links

--- app/about/info.tsx
+++ app/about/info.tsx
@@ -22,7 +22,7 @@ interface InfoDisplayProps {
 export default function InfoDisplay({ info }: InfoDisplayProps) {
   return (
     <main className="grid grid-cols-3 xl:grid-cols-4 p-12">
-      <header className="col-span-3 xl:col-span-4 mb-2 border-b">
+      <header className="col-span-3 xl:col-span-4 mb-2 border-b border-slate-600 dark:border-white">
         <h1 className="text-3xl text-center uppercase">About Me</h1>
       </header>
 
@@ -40,8 +40,10 @@ export default function InfoDisplay({ info }: InfoDisplayProps) {
       <article className="col-span-3 lg:max-xl:col-span-2 ms-4">
         <ReactMarkdown>{info.bio}</ReactMarkdown>
 
-        <section className="border-2 text-center mt-2">
-          <h3 className="uppercase border-b-2">Links</h3>
+        <section className="border-2 text-center mt-2 border-slate-600 dark:border-white">
+          <h3 className="uppercase border-b-2 border-slate-600 dark:border-white">
+            Links
+          </h3>
           <ul className="grid grid-cols-2">
             {info.externalLinks.map((link) => (
               <a
@@ -51,7 +53,9 @@ export default function InfoDisplay({ info }: InfoDisplayProps) {
                 className="uppercase"
                 key={link.label}
               >
-                <li className="border-2 rounded m-1">{link.label}</li>
+                <li className="border-2 rounded m-1 border-slate-600 dark:border-white hover:border-black hover:text-black dark:hover:border-gray-400 dark:hover:text-gray-400">
+                  {link.label}
+                </li>
               </a>
             ))}
           </ul>
