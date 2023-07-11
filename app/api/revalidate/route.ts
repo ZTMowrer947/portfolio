b3b5db6dedabb0d5dd6259d61fbca564b3f33ad5
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-07-11 11:37:13 -0400

    Refactor cache revalidation route

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -3,16 +3,30 @@ import { notFound } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
-  const tag = request.nextUrl.searchParams.get('tag');
+  const type = request.nextUrl.searchParams.get('type');
+  const id = request.nextUrl.searchParams.get('id');
   const token = request.nextUrl.searchParams.get('token');
 
   if (
-    !tag ||
+    !token ||
     process.env.NODE_ENV !== 'production' ||
     token !== process.env.REVALIDATE_TOKEN
   )
     notFound();
 
-  revalidateTag(tag);
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+  if (!type || (type === 'project' && !id))
+    return NextResponse.json(
+      { message: 'Revalidation not supported for published entity' },
+      {
+        status: 400,
+      }
+    );
+
+  if (type === 'project') revalidateTag(`project-${id}`);
+  else if (type === 'author') revalidateTag(type);
+
+  return NextResponse.json({
+    revalidated: ['project', 'author'].includes(type),
+    now: Date.now(),
+  });
 }
