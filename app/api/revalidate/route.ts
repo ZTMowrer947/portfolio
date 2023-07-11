e99cd4d7b5a7de5eccd4805a8b318c56cf8267f4
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-07-10 22:08:37 -0400

    Mask cache revalidation route for non-production routes
    Caching is disabled regardless in all environments except production

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,8 +1,18 @@
-import { revalidatePath } from 'next/cache';
+import { revalidateTag } from 'next/cache';
+import { notFound } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
-  const path = request.nextUrl.searchParams.get('path') || '/';
-  revalidatePath(path);
+export async function POST(request: NextRequest) {
+  const tag = request.nextUrl.searchParams.get('tag');
+  const token = request.nextUrl.searchParams.get('token');
+
+  if (
+    !tag ||
+    process.env.NODE_ENV !== 'production' ||
+    token !== process.env.REVALIDATE_TOKEN
+  )
+    notFound();
+
+  revalidateTag(tag);
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
